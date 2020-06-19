41db640d24fc3dad4a4200f1be926b9ba6ce706f
百科おじさん <tftf161107@gmail.com>
2020-06-19 16:53:35 +0900

    [add] Flutter2Unityのメッセージを実装

--- lib/components/ar_component.dart
+++ lib/components/ar_component.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-class ARComponent extends StatelessWidget {
+class ARComponent extends StatefulWidget {
   static const String id = 'AR';
 
   @override
+  _ARComponentState createState() => _ARComponentState();
+}
+
+class _ARComponentState extends State<ARComponent> {
+  UnityWidgetController _unityWidgetController;
+
+  @override
   Widget build(BuildContext context) {
-    return UnityWidget(
-      onUnityViewCreated: (controller) => {},
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: UnityWidget(onUnityViewCreated: onUnityCreated),
+          flex: 10,
+        ),
+        Expanded(
+          child: Card(
+            child: Center(
+              child: MaterialButton(
+                child: Text('Unityにメッセージを送信'),
+                onPressed: sendMessageToUnity,
+              ),
+            ),
+          ),
+          flex: 1,
+        ),
+      ],
     );
+  }
+
+  void sendMessageToUnity() {
+    _unityWidgetController.postMessage(
+        'GameDirector', 'EditText', 'Flutterからのメッセージ');
+  }
+
+  void onUnityCreated(controller) {
+    this._unityWidgetController = controller;
   }
 }
